d32611d4eeb2d8b5cbb991faaeaf67e2c597f483
Sean Little <seanchristopherlittle@gmail.com>
2021-12-23 00:21:12 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function changeSrc(img){
+    img.src = "assets/succulents-2.jpg";
+}
+function changeSrcBack(img){
+    img.src = "assets/succulents-1.jpg";
+}
+
+// function changeSrc(id) {
+//     var changeImg = document.getElementById(id)
+//     changeImg = "assets/succulents-2.jpg";
+// }
+// function changeSrcBack(id) {
+//     var changeImg = document.getElementById(id)
+//     changeImg = "assets/succulents-1.jpg";
+// }
+
+function hidePopUp(el) {
+    el.parentElement = 'none';
+}
+
+    
